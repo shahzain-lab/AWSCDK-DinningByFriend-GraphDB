f2110f7f3353ea5c58f241b1b40fcc49ab16115c
Syed ZainUlAbideen <shahzain30196@gmail.com>
2022-04-17 20:56:54 +0500

    Initial commit

---
+++ test/awscdk-dinning_by_friend-graph_db.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwscdkDinningByFriendGraphDb from '../lib/awscdk-dinning_by_friend-graph_db-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/awscdk-dinning_by_friend-graph_db-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwscdkDinningByFriendGraphDb.AwscdkDinningByFriendGraphDbStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
