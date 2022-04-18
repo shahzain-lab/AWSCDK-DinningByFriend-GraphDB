6da48533f4ce86473bea41f7ab5adff4a4a8ae1c
Syed ZainUlAbideen <shahzain30196@gmail.com>
2022-04-18 22:49:56 +0500

    yaml deployment

--- bin/awscdk-dinning_by_friend-graph_db.ts
+++ bin/awscdk-dinning_by_friend-graph_db.ts
@@ -2,8 +2,6 @@
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { dinningByFriendMainStack } from '../lib/dinning_by_friend_main-Stack';
-import { dinningByFriendBuildStack } from '../lib/dinning_by_friend_build-Stack';
 
 const app = new cdk.App();
 new dinningByFriendMainStack(app, 'dinningByFriendMainStack');
-new dinningByFriendBuildStack(app, 'dinningByFriendBuildStack');
--- lib/dinning_by_friend_main-Stack.ts
+++ lib/dinning_by_friend_main-Stack.ts
@@ -1,22 +1,46 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import * as cognito from "aws-cdk-lib/aws-cognito";
 import * as appsync from '@aws-cdk/aws-appsync-alpha'
 
 export class dinningByFriendMainStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+     //////////////////////// creating User Pool /////////////////////////////////
+     const userPool = new cognito.UserPool(this, `${this.stackName}_USER_POOL`, {
+      selfSignUpEnabled: true,
+      accountRecovery: cognito.AccountRecovery.EMAIL_ONLY,
+      userVerification: {
+        emailStyle: cognito.VerificationEmailStyle.CODE,
+      },
+      autoVerify: { email: true, },
+      standardAttributes: {
+        email: { required: true, mutable: true, },
+        // phoneNumber: { required: true, mutable: true }
+      },
+    })
+    const userPoolClient = new cognito.UserPoolClient(this, `${this.stackName}_USER_POOL_CLIENT`, {
+      userPool,
+    });
+
+
     const restaurantGraphApi = new appsync.GraphqlApi(this, 'restaurantGraphAPi', {
       name: 'restaurant-graph',
       schema: appsync.Schema.fromAsset('graphql/schema.gql'),
+      
       authorizationConfig: {
         defaultAuthorization: {
          apiKeyConfig: {
            expires: cdk.Expiration.after(cdk.Duration.days(365))
          },
-          authorizationType: appsync.AuthorizationType.API_KEY,
+         userPoolConfig: {
+           userPool
+         },
+          authorizationType: appsync.AuthorizationType.USER_POOL,
         }
       }
+
     })
   }
 }
